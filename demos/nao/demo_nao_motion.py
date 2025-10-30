12e6e4cd8258f5f524a2a8746073f200c9cbf7a9
SofroniaN <sofronia.nicolaou@gmail.com>
2025-10-30 14:20:04 +0100

    nao api

--- demos/nao/demo_nao_motion.py
+++ demos/nao/demo_nao_motion.py
@@ -35,7 +35,7 @@ class NaoMotionDemo(SICApplication):
         super(NaoMotionDemo, self).__init__()
         
         # Demo-specific initialization
-        self.nao_ip = "XXX"
+        self.nao_ip = "10.0.0.242"
         self.nao = None
 
         self.set_log_level(sic_logging.INFO)
